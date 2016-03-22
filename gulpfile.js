3430d59c17d8474b00caa42b2a7d92353aacc1a2
Horst Xu <271021733@qq.com>
2016-03-22 16:55:34 +0800

    file format refined

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,23 @@ var rename = require('gulp-rename');
 
 //compress jquery plugin source code and put it into corresponding folders
 gulp.task('compress-scripts', function(){
-	return gulp.src(['src/jquery.previewer.js'])
-			   .pipe(gulp.dest('dest'))
-			   .pipe(uglify({preserveComments: 'license'}))
-			   .pipe(rename({
-			   		suffix: '.min', 
-			   		extname: '.js'
-			   	}))
-			   .pipe(gulp.dest('dest'))
-			   .pipe(gulp.dest('public/js'))
-			   .pipe(notify({message: 'compress-scripts completed!'}));
+    return gulp.src(['src/jquery.previewer.js'])
+               .pipe(gulp.dest('dest'))
+               .pipe(uglify({preserveComments: 'license'}))
+               .pipe(rename({
+                    suffix: '.min', 
+                    extname: '.js'
+                }))
+               .pipe(gulp.dest('dest'))
+               .pipe(gulp.dest('public/js'))
+               .pipe(notify({message: 'compress-scripts completed!'}));
 });
 
 //lint script
 gulp.task('lint', function(){
-	return gulp.src('src/jquery.previewer.js')
-			   .pipe(jshint())
-			   .pipe(jshint.reporter('default'));
+    return gulp.src('src/jquery.previewer.js')
+               .pipe(jshint())
+               .pipe(jshint.reporter('default'));
 });
 
 gulp.task('default', ['compress-scripts']);
